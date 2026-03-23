f7a2be08f71ff2da29886beb8ebe0d7be80c69ed
Simsayed <159289766+Simsayed@users.noreply.github.com>
2026-03-24 01:45:20 +0530

    Update ann.py

--- ann.py
+++ ann.py
@@ -7,12 +7,16 @@ y = np.array([[4]])
 # Initialize weight
 w = np.random.rand(1,1)
 
-lr = 0.01
+lr = 0.01  #learning rate 
 
 for i in range(100):
-    y_pred = x * w
-    loss = (y - y_pred)**2
-    grad = -2 * x * (y - y_pred)
-    w = w - lr * grad
+    y_pred = x * w      # forward propogation 
+    loss = (y - y_pred)**2   # loss error using mse
+    grad = -2 * x * (y - y_pred) # gradient
+    w = w - lr * grad # update weight
 
-print("Final weight:", w)
+print("InitialWeight:", w)
+print("Loss:", loss)
+print("Predicted Output:", y_pred)
+print("Gradient:", grad)
+
