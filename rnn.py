860c5046bc071f4898cd9b7132b352c59cce10ab
Simsayed <159289766+Simsayed@users.noreply.github.com>
2026-03-26 11:27:40 +0530

    Update rnn.py

--- rnn.py
+++ rnn.py
@@ -2,8 +2,8 @@ import numpy as np
 
 inputs = [1, 2, 3]
 
-W = np.random.rand()
-h = 0
+W = np.random.rand() #weight of i/p reandomly initialised
+h = 0 #hidden state
 
 for x in inputs:
     h = np.tanh(W * x + h)
